e1bc2d71465f97572182468546ccb4a0b702c1f9
Lada jjkkrrxx <perehodchenko.lada@gmail.com>
2026-09-19 23:56:04 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,38 +1,61 @@
 import express from 'express';
 import 'dotenv/config';
+import PinoHttp from 'pino-http';
 import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import { errors as celebrateErrors } from 'celebrate';
-import { connectMongoDB } from './db/connectMongoDB.js';
-import { logger } from './middleware/logger.js';
-import { notFoundHandler } from './middleware/notFoundHandler.js';
-import { errorHandler } from './middleware/errorHandler.js';
-import notesRoutes from './routes/notesRoutes.js';
-import authRoutes from './routes/authRoutes.js';
-import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
 
-await connectMongoDB();
-
-app.use(logger);
 app.use(express.json());
-app.use(
-  cors({
-    origin: true,
-    credentials: true,
-  }),
-);
-app.use(cookieParser());
 
-app.use(authRoutes);
-app.use(notesRoutes);
-app.use(userRoutes);
+const logger = PinoHttp({
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      colorize: true,
+      translateTime: 'HH:MM:ss',
+      ignore: 'pid,hostname',
+      messageFormat:
+        '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+      hideObject: true,
+    },
+  },
+});
+app.use(logger);
 
-app.use(celebrateErrors());
-app.use(notFoundHandler);
-app.use(errorHandler);
+app.use(cors());
+
+
+app.get('/notes', (req, res) => {
+  res.status(200).json({
+    message: 'Retrieved all notes',
+  });
+});
+
+app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
+  res.status(200).json({
+    message: `Retrieved note with ID: ${noteId}`,
+  });
+});
+
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
+});
+
+app.use((req, res) => {
+  res.status(404).json({
+    message: 'Route not found',
+  });
+});
+
+app.use((error, req, res, next) => {
+  const isProd = process.env.NODE_ENV === 'production';
+  const message = isProd ? 'Some error' : error.message;
+  res.status(500).json({
+    message,
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
