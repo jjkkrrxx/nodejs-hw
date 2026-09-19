2dd6f6c3445a43886be9cf40a1b50a87460af65e
Lada jjkkrrxx <perehodchenko.lada@gmail.com>
2026-09-20 00:19:52 +0200

    fix_2

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import { errors as celebrateErrors } from 'celebrate';
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
+import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
@@ -14,10 +18,23 @@ await connectMongoDB();
 
 app.use(logger);
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  }),
+);
+app.use(cookieParser());
 
+app.use(authRoutes);
 app.use(notesRoutes);
+app.use(userRoutes);
 
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
+});
+
+app.use(celebrateErrors());
 app.use(notFoundHandler);
 app.use(errorHandler);
 
