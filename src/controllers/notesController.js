9542adeeb5c3e68ad4a78781824a7bf9c225fd89
Lada jjkkrrxx <perehodchenko.lada@gmail.com>
2026-09-20 00:41:26 +0200

    fix_1

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -2,8 +2,40 @@ import createHttpError from 'http-errors';
 import { Note } from '../models/note.js';
 
 export const getAllNotes = async (req, res) => {
-  const notes = await Note.find();
-  res.status(200).json(notes);
+  const { page, perPage, tag, search } = req.query;
+  const pageNumber = Number(page) || 1;
+  const perPageNumber = Number(perPage) || 10;
+  const skip = (pageNumber - 1) * perPageNumber;
+
+  const notesQuery = Note.find();
+
+  if (tag) {
+    notesQuery.where('tag').equals(tag);
+  }
+
+  if (search) {
+    notesQuery.where({
+      $or: [
+        { title: { $regex: search, $options: 'i' } },
+        { content: { $regex: search, $options: 'i' } },
+      ],
+    });
+  }
+
+  const [notes, totalNotes] = await Promise.all([
+    notesQuery.clone().skip(skip).limit(perPageNumber),
+    notesQuery.clone().countDocuments(),
+  ]);
+
+  const totalPages = Math.ceil(totalNotes / perPageNumber);
+
+  res.status(200).json({
+    page: pageNumber,
+    perPage: perPageNumber,
+    totalNotes,
+    totalPages,
+    notes,
+  });
 };
 
 export const getNoteById = async (req, res) => {
--- src/server.js
+++ src/server.js
@@ -1,53 +1,23 @@
 import express from 'express';
 import 'dotenv/config';
-import PinoHttp from 'pino-http';
 import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import { errors as celebrateErrors } from 'celebrate';
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
-import authRoutes from './routes/authRoutes.js';
-import userRoutes from './routes/userRoutes.js';
+import { errors as celebrateErrors } from 'celebrate';
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
 
-app.use(express.json());
+await connectMongoDB();
 
-const logger = PinoHttp({
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
-      translateTime: 'HH:MM:ss',
-      ignore: 'pid,hostname',
-      messageFormat:
-        '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
-      hideObject: true,
-    },
-  },
-});
 app.use(logger);
 app.use(express.json());
-app.use(
-  cors({
-    origin: true,
-    credentials: true,
-  }),
-);
-app.use(cookieParser());
+app.use(cors());
 
-app.use(authRoutes);
 app.use(notesRoutes);
-app.use(userRoutes);
-
-app.get('/test-error', () => {
-  throw new Error('Simulated server error');
-});
-
 app.use(celebrateErrors());
 app.use(notFoundHandler);
 app.use(errorHandler);
