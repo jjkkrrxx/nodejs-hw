b4cfa404e90d16ed9ba0e9e9c29d1cc1fb7e6de5
Lada jjkkrrxx <perehodchenko.lada@gmail.com>
2026-09-20 02:05:31 +0200

    commit

--- src/server.js
+++ src/server.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import { errors as celebrateErrors } from 'celebrate';
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
-import { errors as celebrateErrors } from 'celebrate';
+import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
@@ -15,9 +18,18 @@ await connectMongoDB();
 
 app.use(logger);
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  }),
+);
+app.use(cookieParser());
 
+app.use(authRoutes);
 app.use(notesRoutes);
+app.use(userRoutes);
+
 app.use(celebrateErrors());
 app.use(notFoundHandler);
 app.use(errorHandler);
